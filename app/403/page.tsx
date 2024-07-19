f6592e387f33f5eaf0a455ea189f4901db0892df
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-07-19 17:45:08 +0630

    update text align

--- app/403/page.tsx
+++ app/403/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Forbidden() {
   return (
-    <div className="h-screen flex justify-center flex-col items-center">
+    <div className="h-screen flex justify-center flex-col items-center px-3 text-center">
         <h3 className="text-red-600 font-light text-3xl uppercase">Missing credentials</h3>
         <div>You are not allowed to see it.... Contact the following person: </div>
         <code className='py-2'>
